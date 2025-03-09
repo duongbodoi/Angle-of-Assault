72a6d88880736a38267330dd0556983018e7a829
ADMIN <ADMIN@ADMIN-PC>
2025-03-09 15:08:58 +0700

    update 02 : các chuyển động cơ bản trong game( mũi tên để di chuyển), 'R' để ném( ném xiên(quỹ đạo cong))

--- Angle of Assault/main.cpp
+++ Angle of Assault/main.cpp
@@ -36,6 +36,178 @@ void close() {
 	window = NULL;
 	SDL_QUIT;
 }
+class Ldot {
+public:
+	const int screendot_w = 20;
+	const int screendot_h = 20;
+	const int dot_vel = 10;
+	Ldot();
+	void handle_event(SDL_Event* e);
+	void render();
+	void move();
+
+private:
+	int velx, vely;
+	int movex, movey;
+
+};
+Ldot::Ldot() {
+	velx = 0;
+	vely = 0;
+	movex = 0;
+	movey = 0;
+
+}
+void Ldot::handle_event(SDL_Event* e) {
+	if (e->type == SDL_KEYDOWN && e->key.repeat == 0) {
+		switch (e->key.keysym.sym)
+		{
+		case SDLK_UP: vely -= dot_vel; break;
+		case SDLK_DOWN: vely += dot_vel; break;
+		case SDLK_LEFT: velx -= dot_vel; break;
+		case SDLK_RIGHT: velx += dot_vel; break;
+
+		default:
+			break;
+		}
+	}
+	else if (e->type == SDL_KEYUP && e->key.repeat == 0) {
+		switch (e->key.keysym.sym)
+		{
+		case SDLK_UP: vely += dot_vel; break;
+		case SDLK_DOWN: vely -= dot_vel; break;
+		case SDLK_LEFT: velx += dot_vel; break;
+		case SDLK_RIGHT: velx -= dot_vel; break;
+
+		default:
+			break;
+		}
+	}
+}
+void Ldot::move() {
+	movex += velx;
+	if (movex < 0 or movex + screendot_w>1160) {
+		movex -= velx;
+	}
+	movey += vely;
+	if (movey < 0 or movey + screendot_h>800) {
+		movey -= vely;
+	}
+}
+
+Ldot dot;
+void Ldot::render() {
+	SDL_Rect offset = { movex,movey,screendot_w,screendot_h };
+	SDL_RenderFillRect(renderer, &offset);
+}
+class Dshot {
+public:
+	int shot_w = 30, shot_h = 30;
+	int v = 100;
+	Dshot();
+	void handle_event(SDL_Event* e);
+	void fire();
+	void render();
+private:
+	int amo_x, amo_y;
+	bool reload;
+	bool start;
+	double time;
+};
+Dshot shot;
+Dshot::Dshot() {
+	amo_x = 0;
+	amo_y = 0;
+	reload = true;
+	start = false;
+	time = 0;
+}
+void Dshot::render() {
+	SDL_Rect offset = { amo_x,amo_y,shot_w,shot_h };
+	SDL_RenderFillRect(renderer, &offset);
+}
+void Dshot::handle_event(SDL_Event* e) {
+	if (e->type == SDL_KEYDOWN) {
+		switch (e->key.keysym.sym)
+		{
+		case SDLK_r: {
+			reload = true;
+			start = false;
+			break;
+		}
+		case SDLK_f: {
+			reload = false;
+			start = true;
+			break;
+		}
+
+		}
+
+	}
+	if (e->type == SDL_KEYDOWN) {
+		switch (e->key.keysym.sym)
+		{
+		case SDLK_r: {
+			reload = false;
+			start = true;
+			break;
+		}
+		case SDLK_f: {
+			reload = true;
+			start = false;
+			break;
+		}
+
+		}
+
+
+
+
+	}
+}
+void Dshot::fire() {
+	double phi = 60;
+	double radian = phi * PI / 180;
+
+	if (reload) {
+		time = 0;
+		amo_x = 0;
+		amo_y = 600;
+	}
+	if (start) {
+		time += 0.1;
+		amo_x = v * cos(radian) * time;;
+		amo_y = 600 - (v * sin(radian) * time - 0.5 * 9.8 * time * time);
+		if (amo_y >= 600) start = false;
+	}
+
+}
 int main(int argv, char* argc[]) {
+	if (!Init()) {
+		cout << "error init";
+	}
+	else {
+		bool running = true;
+		SDL_Event e;
+		while (running) {
+			while (SDL_PollEvent(&e) != 0) {
+				if (e.type == SDL_QUIT) {
+					running = false;
+				}
+				dot.handle_event(&e);
+				shot.handle_event(&e);
+			}
+			dot.move();
+			shot.fire();
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+			SDL_RenderClear(renderer);
+
+			SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+			dot.render();
+			shot.render();
+			SDL_RenderPresent(renderer);
+		}
+		close();
+	}
 	return 0;
 }
